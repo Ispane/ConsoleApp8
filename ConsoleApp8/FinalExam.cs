a78e5daf1b0bd45323bf88db4908b79e4247aa54
Admin <Admin@DESKTOP-9H2KH00>
2022-02-04 10:44:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/FinalExam.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    abstract class FinalExam
+    {
+        public abstract double GetEstimation();
+    }
+
+    class Test : FinalExam
+    {
+        public float Score { get; set; }
+                             
+        public override double GetEstimation() => Score / 20;
+    }
+
+    class Exam : FinalExam
+    {
+        public float SumEst { get; set; }
+        public float Quantity { get; set; }
+
+        public override double GetEstimation() => SumEst / Quantity;
+    }
+
+    
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FinalExam test = new Test { Score = 80};
+            FinalExam exam = new Exam { SumEst = 9, Quantity = 3};
+
+            PrintFinalExam(test); 
+            PrintFinalExam(exam);
+
+            void PrintFinalExam(FinalExam FinalExam)
+            {
+                Console.WriteLine($"Оценка:  {FinalExam.GetEstimation()}");
+            }
+        }
+    }
+}
+
